f34cb8c712b242a03733d5220fb6844591ce268b
StarcevAlexander <116572797+StarcevAlexander@users.noreply.github.com>
2026-03-24 15:40:10 +0800

    Auto-scroll to latest message on every list update

---
+++ messenger/.angular/cache/21.2.3/messenger/vite/deps/@angular_cdk_bidi.js
@@ -0,0 +1,13 @@
+import {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+} from "./chunk-I3LOXW57.js";
+import "./chunk-RXUDNG3Z.js";
+export {
+  BidiModule,
+  DIR_DOCUMENT,
+  Dir,
+  Directionality
+};
---
+++ messenger/.angular/cache/21.2.3/messenger/vite/deps/@angular_cdk_scrolling.js
@@ -0,0 +1,49 @@
+import {
+  CDK_VIRTUAL_SCROLL_VIEWPORT,
+  CdkFixedSizeVirtualScroll,
+  CdkScrollable,
+  CdkScrollableModule,
+  CdkVirtualForOf,
+  CdkVirtualScrollViewport,
+  CdkVirtualScrollable,
+  CdkVirtualScrollableElement,
+  CdkVirtualScrollableWindow,
+  DEFAULT_RESIZE_TIME,
+  DEFAULT_SCROLL_TIME,
+  FixedSizeVirtualScrollStrategy,
+  ScrollDispatcher,
+  ScrollingModule,
+  VIRTUAL_SCROLLABLE,
+  VIRTUAL_SCROLL_STRATEGY,
+  ViewportRuler,
+  _fixedSizeVirtualScrollStrategyFactory
+} from "./chunk-2MTJR23L.js";
+import "./chunk-GUGIMSVJ.js";
+import {
+  Dir
+} from "./chunk-I3LOXW57.js";
+import "./chunk-BYKYBSXN.js";
+import "./chunk-INZXTVON.js";
+import "./chunk-FEK6FJDQ.js";
+import "./chunk-RXUDNG3Z.js";
+export {
+  CDK_VIRTUAL_SCROLL_VIEWPORT,
+  CdkFixedSizeVirtualScroll,
+  CdkScrollable,
+  CdkScrollableModule,
+  CdkVirtualForOf,
+  CdkVirtualScrollViewport,
+  CdkVirtualScrollable,
+  CdkVirtualScrollableElement,
+  CdkVirtualScrollableWindow,
+  DEFAULT_RESIZE_TIME,
+  DEFAULT_SCROLL_TIME,
+  FixedSizeVirtualScrollStrategy,
+  ScrollDispatcher,
+  ScrollingModule,
+  VIRTUAL_SCROLLABLE,
+  VIRTUAL_SCROLL_STRATEGY,
+  ViewportRuler,
+  _fixedSizeVirtualScrollStrategyFactory,
+  Dir as ɵɵDir
+};
